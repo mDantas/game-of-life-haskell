9cca71c932f2d95d10a7add6a5b49e2cd9eeb4ea
Matthew Dantas-McCutcheon <mdantasmccutcheon@vistaprint.net>
2018-01-24 16:09:27 -0500

    Add ability to get neighbors of a cell from the board & a position

--- lib.hs
+++ lib.hs
@@ -1,8 +1,10 @@
 data Cell = Alive | Dead deriving (Show, Eq)
 type Cells = [Cell]
 
+type Board = [[Cell]]
+
 census :: Cells -> Int
-census = (length . filter . (== Alive))
+census = (length . filter (== Alive))
 
 fate :: Cell -> Int -> Cell
 fate Dead 3 = Alive
@@ -11,3 +13,12 @@ fate Alive nAlive
   | nAlive < 2 = Dead --underpopulation
   | nAlive > 3 = Dead --overpopulation
   | otherwise = Alive
+
+cellAt :: Board -> (Int, Int) -> Cell
+cellAt b (i,j) = b !! i !! j
+
+neighbors :: Board -> (Int,Int) -> Cells
+neighbors b = map (cellAt b) . positions
+  where
+    moves = [(+0),(+(-1)),(+1)]
+    positions (i,j) = tail ((,) <$> (moves [i]) <*> (moves [j])) -- tail to exclude the cell itself which will be at the head
