04e3dd460b44cf2825c59fed20a1c9e48932de6f
Matthew Dantas-McCutcheon <mdantasmccutcheon@vistaprint.net>
2018-01-25 15:41:26 -0500

    General cleanup

--- src/board.hs
+++ src/board.hs
@@ -1,9 +1,9 @@
 module Board where
 
+-- Use vectors so we have O(1) random access (to get a cells neighbors).
 import Data.Vector as V
 import Data.Vector.Split
 import Data.List
--- import Data.List.Split
 
 data Cell = Alive | Dead deriving (Eq)
 instance Show Cell where
@@ -14,20 +14,20 @@ type Cells = Vector Cell
 
 type Board = Vector (Vector Cell)
 
--- From a list of cells, get the number that are alive.
+-- From a vector of cells, get the number that are alive.
 census :: Cells -> Int
 census = V.length . V.filter (== Alive)
 
 -- The basic rules for Conway's Game of Life
 fate :: Cell -> Int -> Cell
-fate Dead 3 = Alive
-fate Dead _ = Dead
+fate Dead 3 = Alive   -- regeneration
+fate Dead _ = Dead    -- steady state dead
 fate Alive nAlive
-  | nAlive < 2 = Dead --underpopulation
-  | nAlive > 3 = Dead --overpopulation
-  | otherwise = Alive
+  | nAlive < 2 = Dead -- underpopulation
+  | nAlive > 3 = Dead -- overpopulation
+  | otherwise = Alive -- steady state alive
 
--- Get cell at coordinates
+-- Get cell at coordinates.
 cellAt :: Board -> (Int, Int) -> Cell
 cellAt b (i,j) = b ! i ! j
 
@@ -41,8 +41,9 @@ neighbors b = V.map (cellAt b) . legal . V.fromList . positions
     moves = [(+0),(+(-1)),(+1)]
     positions (i,j) = Data.List.tail ((,) <$> (moves <*> [i]) <*> (moves <*> [j])) -- tail to exclude the cell itself which will be at the V.head
 
+-- Compute the fate of a cell at position p on the board.
 fateAt :: Board -> (Int, Int) -> Cell
-fateAt b p@(i,j) = fate (cellAt b p) (census (neighbors b p))
+fateAt b p = fate (cellAt b p) (census (neighbors b p))
 
 -- Get the next generation of the board
 -- (I.e. apply 'fate' to every element on the board)
@@ -54,5 +55,7 @@ nextGen b = V.fromList (chunksOf (V.length (V.head b)) (V.map (fateAt b) positio
 showBoard :: Board -> String
 showBoard b = intercalate "\n" (V.toList (V.map (Data.List.concatMap show) b))
 
+-- Take an int (width) and a list of cells, return a vector of vectors of cells
+-- where each vector of cells is of length width.
 toBoard :: Int -> [Cell] -> Board
 toBoard width cs = V.fromList (chunksOf width (V.fromList cs))
